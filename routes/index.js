9a4ab574885835569b29c7377c1288786582b445
Brenden Thomas <brenden.thomas17@gmail.com>
2022-09-12 18:18:27 -0500

    cleaned up original landing code

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var mongoDB = 'mongodb+srv://bthomas:bthomas@cluster0.nkamwxm.mongodb.net/?retryWrites=true&w=majority'
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
-const bodyParser = require('body-parser');
+const Posts = require('body-parser');
 router.use(bodyParser.json());
 
 const UserData = require('../models/documentSchema')
@@ -9,32 +9,11 @@ var router = express.Router();
 
 /* GET home page. */
 
-router.get('/', function(req,res, next){
-  Posts.findById("631fb302c065ac636b9c5f95", (err, data)=>{
-    res.header("Access-Control-Allow-Origin", "https://batfe.herokuapp.com");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.send("HAH! GOTEM!")
-    
+router.get('/', async(req,res, next) =>{
+  const currentPosts = await Posts.findById("62e965869ada8f801e5443ea", (err, data)=>{
     if(err) {return console.error(err)}
-    /*
-    const newPost = {
-      public:true,
-      image: req.body.image,
-      title: req.body.title,
-      blogPost: req.body.blogPost,
-      comments: [],
-      //new dates dont appear to be saving in DB but are showing in client front-end.
-      date: new Date(),
-    }*/
-    
-
-    //data.posts.push(newPost)
-
-    //data.save((err,updatedData)=>{
-    //  if(err) console.error(err)
-    //})
   }).clone()
-  res.send("Lol what?");
+  res.json(currentPosts)
 
 })
 
