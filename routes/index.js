a5771a9131f3c04cdf5c42b5a6d6f97dff2256f4
Brenden Thomas <brenden.thomas17@gmail.com>
2022-09-12 19:30:17 -0500

    ggg

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,8 @@ router.get('/', async(req,res, next) =>{
   const currentPosts = await UserData.findById("631fb302c065ac636b9c5f95", (err, data)=>{
     if(err) {return console.error(err)}
   }).clone()
-  res.json(currentPosts)
+  console.log(res.json(currentPosts));
+  res.send("Hi")
 
 })
 
