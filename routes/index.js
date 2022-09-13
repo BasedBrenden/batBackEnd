c6699acb8c5bdb523dddd511eb9ece7dd000d5ee
Brenden Thomas <brenden.thomas17@gmail.com>
2022-09-12 19:32:20 -0500

    Final edit for back end?

--- routes/index.js
+++ routes/index.js
@@ -13,8 +13,8 @@ router.get('/', async(req,res, next) =>{
   const currentPosts = await UserData.findById("631fb302c065ac636b9c5f95", (err, data)=>{
     if(err) {return console.error(err)}
   }).clone()
-  console.log(res.json(currentPosts));
-  res.send("Hi")
+  res.json(currentPosts)
+  
 
 })
 
