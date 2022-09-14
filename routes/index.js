a8d108e0669095aa37273db43eda2e64976ab63e
Brenden Thomas <brenden.thomas17@gmail.com>
2022-09-14 15:01:14 -0500

    Reintegrated .clone() to get request

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ var router = express.Router();
 router.get('/', async(req,res, next) =>{
   const currentPosts = await UserData.findById("631fb302c065ac636b9c5f95", (err, data)=>{
     if(err) {return console.error(err)}
-  })
+  }).clone();
   res.json(currentPosts)
   
 
