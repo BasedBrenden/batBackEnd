3af2962eb68439aaa27adbb11c4931193c2b3823
Brenden Thomas <brenden.thomas17@gmail.com>
2022-09-14 15:59:14 -0500

    another

--- app.js
+++ app.js
@@ -3,20 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-
+const cors = require('cors');
 var app = express();
 
-const cors = require('cors');
 app.use(cors());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods: GET, PUT, POST, OPTIONS');
+  res.header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
+var indexRouter = require('./routes/index');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
