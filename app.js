9e8c0c9e1e2caec2265b7d7016bcbedf645a39b6
Brenden Thomas <brenden.thomas17@gmail.com>
2022-09-14 16:39:05 -0500

    try again

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var app = express();
 app.use(cors());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
