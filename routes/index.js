68ad28a7425c86f3bee95a8a9952b0edac0cdc3a
Brenden Thomas <brenden.thomas17@gmail.com>
2022-09-14 14:58:53 -0500

    Added HTTPS PUT func

--- routes/index.js
+++ routes/index.js
@@ -12,9 +12,23 @@ var router = express.Router();
 router.get('/', async(req,res, next) =>{
   const currentPosts = await UserData.findById("631fb302c065ac636b9c5f95", (err, data)=>{
     if(err) {return console.error(err)}
-  }).clone()
+  })
   res.json(currentPosts)
   
+
+})
+
+/* PUT a new pokemon into the roster*/
+
+router.put('/api/put', (req,res)=>{
+
+  UserData.findById("631fb302c065ac636b9c5f95", (err, data)=>{
+    res.header("Access-Control-Allow-Origin", "https://batbackend.herokuapp.com");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    if(err) {return console.error(err)}
+    const testReq = req.body.newPokemon;
+    data.Team.push(testReq)
+  })
 
 })
 
