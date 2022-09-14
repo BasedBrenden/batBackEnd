52e81154be249d0eac18f6f8fe4438e7cfe80aa4
Brenden Thomas <brenden.thomas17@gmail.com>
2022-09-14 18:00:37 -0500

    the default has arrived

--- app.js
+++ app.js
@@ -3,15 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const cors = require('cors');
+var cors = require('cors');
 var app = express();
 
-app.use(function(req, res, next) {
+
+app.use(cors());
+
+/*app.use(function(req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-});
+});*/
 
 var indexRouter = require('./routes/index');
 // view engine setup
