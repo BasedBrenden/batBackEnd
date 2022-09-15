12dccfdca87b6f417c19ac845d531da90b44fb7c
Brenden Thomas <brenden.thomas17@gmail.com>
2022-09-14 21:28:37 -0500

    Added template for delete func

--- routes/index.js
+++ routes/index.js
@@ -25,9 +25,6 @@ router.get('/', async(req,res, next) =>{
 /* PUT a new pokemon into the roster*/
 
 router.post('/apir', (req,res, next)=>{
-  /*res.setHeader("Access-Control-Allow-Origin", '*');
-  res.setHeader("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content-Type, Accept');
-  */
   const newPokemonInfo ={
     pokeID: req.body.pokeID,
     pokeImage: req.body.pokeImage,
@@ -46,6 +43,29 @@ router.post('/apir', (req,res, next)=>{
   })
   
   res.send();
+})
+
+/* Post: delete specific pokemon from team
+*/
+
+router.post('/apir/delete', (req,res)=>{
+
+  const pokemonID = req.body.id;
+  UserData.findById("631fb302c065ac636b9c5f95", (err, data)=>{
+    if(err) {return console.error(err)}
+  
+    //remove from data, then save updated version to mongoDB
+    data.Team.splice(pokemonID, 1)
+    data.save((err, updateD)=>{
+      if(err) console.error(err)
+    })
+
+    
+  })
+
+  res.send();
+  
+
 })
 
 module.exports = router;
