4cdb90118f638ff014ac2e0bcb9537eb8be92500
Brenden Thomas <brenden.thomas17@gmail.com>
2022-09-23 20:32:17 -0500

    testing out post request

--- routes/index.js
+++ routes/index.js
@@ -16,9 +16,9 @@ router.get('/', async(req,res, next) =>{
 })
 
 
-router.get('/user/:userId', async(req,res, next) =>{
+router.post('/', async(req,res, next) =>{
 
-  const username = req.query.userId
+  const username = req.body.userId
   const currentPosts = await UserData.find({Username: username}, (err, data)=>{
     if(err) {return console.error(err)}
   }).clone();
